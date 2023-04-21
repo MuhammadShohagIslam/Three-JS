53f71977a5af6fb1f28929d66c5056b879bdafeb
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2023-04-21 20:57:20 +0300

    practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { Canvas } from "@react-three/fiber"
 const App = () => {
   return (
-    <>
-
-    </>
+    <div>
+      <Canvas>
+        <pointLight position={[10, 10, 10]} />
+        <mesh>
+          <sphereGeometry args={[15, 32, 16]} />
+          <meshBasicMaterial />
+        </mesh>
+      </Canvas>
+    </div>
   )
 }
 
